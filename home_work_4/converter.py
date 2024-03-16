0865c9aac7c3bdaac226e7a7e55981fe8876b302
VladyslavKanarkin <162754486+VladyslavKanarkin@users.noreply.github.com>
2024-03-16 23:31:28 +0100

    Update converter.py

--- home_work_4/converter.py
+++ home_work_4/converter.py
@@ -1,19 +1,9 @@
-
-EXCHANGE_RATES = {
-    "EUR": 50.1,
-    "USD": 45.1,
-    "UAN": 30.1
-}
-
-def check(currency):
-    if currency not in EXCHANGE_RATES: raise ValueError("Неизвестная валюта " + currency)
-
-def convert(curr_from, curr_to, amount):
-    check(curr_from); check(curr_to)
-    return amount * EXCHANGE_RATES[curr_from] / EXCHANGE_RATES[curr_to]
-
-cf = input('Currency1:')
-ct = input('Currency2:')
-amt = int(input('Quantity:'))
-
-print(convert(cf, ct, amt))
+amount = float(input("Enter amount:"))
+uah_usd = amount / 40.50
+usd_uah = amount * 40.50
+uah_eur = amount / 44.40
+eur_uah = amount * 44.40
+print(f"UAH --> USD: {uah_usd}. \n"
+      f"USD --> UAH: {usd_uah}. \n"
+      f"UAH --> EUR: {uah_eur}. \n"
+      f"EUR --> UAH: {eur_uah}.")
