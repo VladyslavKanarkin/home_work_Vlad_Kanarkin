a93810117345ed0883e5ea3a94a426f143bcd411
VladyslavKanarkin <vladyslavkanarkin@gmail.com>
2024-03-22 11:50:00 +0100

    i hope everything worked out for me

--- home_work_4/converter.py
+++ home_work_4/converter.py
@@ -1,19 +1,10 @@
+amount = float(input("Enter amount:"))
+uah_usd = amount / 35.50
+usd_uah = amount / 35.50
+uah_eur = amount / 38.57
+eur_uah = amount / 38.57
+print(f"UAH --> USD: {uah_usd}, \n"
+      f"USD --> UAH: {usd_uah}, \n"
+      f"UAH --> EUR: {uah_eur}, \n"
+      f"EUR --> UAH: {eur_uah},")
 
-EXCHANGE_RATES = {
-    "EUR": 50.1,
-    "USD": 45.1,
-    "UAN": 30.1
-}
-
-def check(currency):
-    if currency not in EXCHANGE_RATES: raise ValueError("Неизвестная валюта " + currency)
-
-def convert(curr_from, curr_to, amount):
-    check(curr_from); check(curr_to)
-    return amount * EXCHANGE_RATES[curr_from] / EXCHANGE_RATES[curr_to]
-
-cf = input('Currency1:')
-ct = input('Currency2:')
-amt = int(input('Quantity:'))
-
-print(convert(cf, ct, amt))
---
+++ home_work_5/sorting.py
@@ -0,0 +1,8 @@
+
+my_list = ['Tst', 'aBc', 'TEST', 'Hello', 'neW']
+#sorted_words = sorted(my_list, key=str.lower())
+my_list.sort(key=len)
+print(my_list)
+#print(sorted_words)
+
+
